02399a1da33f31f0b72cc5c3a67dfa27cb5bbea8
zhaoqiming <zhaoqm@didi-soft.com>
2020-07-31 15:18:17 +0800

    修改1

---
+++ src/main/java/com/example/test/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.test.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+                http.authorizeRequests()
+                .antMatchers("/product/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                // 管理员，同事具有 ADMIN,USER权限，可以访问所有资源
+                .withUser("admin1")
+                .password("{noop}admin1")
+                .roles("ADMIN", "USER")
+                .and()
+                // 普通用户，只能访问 /product/**
+                .withUser("user1").password("user1")
+                .roles("USER");
+    }
+
+
+}
---
+++ src/main/java/com/example/test/controller/ProductTestController.java
@@ -0,0 +1,24 @@
+package com.example.test.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/product")
+public class ProductTestController {
+    @RequestMapping("/info")
+    @ResponseBody
+    public String productInfo(){
+        String currentUser = "";
+        Object principl = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principl instanceof UserDetails) {
+            currentUser = ((UserDetails)principl).getUsername();
+        }else {
+            currentUser = principl.toString();
+        }
+        return " some product info,currentUser is: "+currentUser;
+    }
+}
