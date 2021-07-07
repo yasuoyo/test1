21655b8fb211fa81efb2b19ac6a661d82176a9d4
zhaoqiming <zhaoqm@didi-soft.com>
2021-07-07 11:04:35 +0800

    oss/minio图片上传

---
+++ src/main/java/com/example/test/upload/api/impl/SysBaseApiImpl.java
@@ -0,0 +1,71 @@
+package com.example.test.upload.api.impl;
+
+
+import com.example.test.upload.api.ISysBaseAPI;
+import com.example.test.upload.utils.MinioUtil;
+import com.example.test.upload.utils.OssBootUtil;
+import com.example.test.upload.utils.OssConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Description: 底层共通业务API，提供其他独立模块调用
+ * @Author: hfx
+ * @Version:V1.0
+ */
+@Slf4j
+@Service
+@Component
+public class SysBaseApiImpl implements ISysBaseAPI {
+	/** 当前系统数据库类型 */
+	private static String DB_TYPE = "";
+
+	/**
+	 * minio桶名称
+	 */
+	private static String miniBucketName;
+	@Value(value = "${upload.minio.bucketName}")
+	public void setMiniBucketName(String miniBucketName) {
+		SysBaseApiImpl.miniBucketName = miniBucketName;
+	}
+	private static String ossBucketName;
+	@Value(value = "${upload.oss.bucketName}")
+	public void setOssBucketName(String ossBucketName) {
+		SysBaseApiImpl.ossBucketName = ossBucketName;
+	}
+
+	@Override
+	public String upload(MultipartFile file, String bizPath, String uploadType) {
+		String url = "";
+		if(OssConstants.UPLOAD_TYPE_MINIO.equals(uploadType)){
+			url = MinioUtil.upload(file,bizPath);
+		}else{
+			url = OssBootUtil.upload(file,bizPath);
+		}
+		return url;
+	}
+
+	@Override
+	public String upload(MultipartFile file, String bizPath, String uploadType, String customBucket) {
+		String url = "";
+		if(OssConstants.UPLOAD_TYPE_MINIO.equals(uploadType)){
+			url = MinioUtil.upload(file,bizPath,customBucket);
+		}else{
+			url = OssBootUtil.upload(file,bizPath,customBucket);
+		}
+		return url;
+	}
+
+	@Override
+	public void delImg(String delType, String delImgName) {
+		if (OssConstants.UPLOAD_TYPE_MINIO.equals(delType)) {
+			MinioUtil.removeObject(miniBucketName, delImgName);
+		} else {
+			OssBootUtil.delete(delImgName);
+		}
+	}
+
+}
---
+++ src/main/java/com/example/test/upload/utils/OssConstants.java
@@ -0,0 +1,21 @@
+package com.example.test.upload.utils;
+
+/**
+ * 常量类
+ *
+ * @author hefan
+ * @date 2019/10/23 11:35
+ */
+public class OssConstants {
+
+
+	/**
+	 * 文件上传类型（本地：local，Minio：minio，阿里云：alioss）
+	 */
+	public static final String UPLOAD_TYPE_LOCAL = "local";
+	public static final String UPLOAD_TYPE_MINIO = "minio";
+	public static final String UPLOAD_TYPE_OSS = "alioss";
+	public static final String UPLOAD_OSS_HTTPS = "https://";
+	public static final String UPLOAD_OSS_SUCCESS = "------OSS文件上传成功------";
+
+}
